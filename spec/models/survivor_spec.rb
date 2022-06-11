70d08e20c67ed99838523eb88f3884e92c61c998
Marco Castro <marcodotcastro@gmail.com>
2022-06-11 08:07:07 -0300

    test(survivor): presence age and gender

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Survivor, type: :model do
   describe 'associations' do
-    it { should belong_to(:city) }
+    it { should belong_to(:city).optional }
   end
 
   describe 'validations' do
-    xit { should validate_presence_of(:age) }
-    xit { should validate_presence_of(:gender) }
+    it { should validate_presence_of(:age) }
+    it { should validate_presence_of(:gender) }
   end
 end
