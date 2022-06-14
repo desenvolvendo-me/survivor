940d9cd1635c67dd39455cf9123e45b3130c5276
Marco Castro <marcodotcastro@gmail.com>
2022-06-14 06:45:14 -0300

    feat(user): add logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'welcome#index'
+
   get 'welcome/index'
   resources :survivors
 
-
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
