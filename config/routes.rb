1acc71a65815f875fd068142312822b8504cc357
Marco Castro <marcodotcastro@gmail.com>
2022-06-14 06:49:46 -0300

    feat(user): add edit account

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :survivors
 
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
